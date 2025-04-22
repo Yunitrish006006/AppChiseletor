e1aa5ea71d6cea29b79319f4ea83fb92dcd9820a
yunthomas006 <yunthomas006@gmail.com>
2025-04-22 05:34:28 +0000

    add

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'theme/theme_manager.dart'; // Import the ThemeManager class
 import 'auth/auth_manager.dart';
-import 'firebase_options.dart';
 import 'plugins/my_custom_theme.dart';
 
 void main() async {
